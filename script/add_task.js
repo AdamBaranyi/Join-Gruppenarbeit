3f04adee243ba92b8895d4097a7b5f5d08c61147
Aliaqa Alizada <alimhd276@gmail.com>
2026-02-21 23:59:38 +0100

    desing & functions added

--- script/add_task.js
+++ script/add_task.js
@@ -1,14 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".trigger");  
+  const buttons = document.querySelectorAll(".trigger");
 
-    buttons.forEach(button => {
-        button.addEventListener("click", function (event) {
-            event.preventDefault();
-
-            buttons.forEach(btn => btn.classList.remove("active"));
-            this.classList.add("active");
-        });
-    })
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      buttons.forEach((btn) => btn.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
 });
 
+// Form Validation-------------------
+const tasktitle = document.getElementById("task-title");
+const taskdate = document.getElementById("due-datet");
 
+function validateForm() {
+  let isValid = true;
+
+  if (!tasktitle.value.trim()) {
+    setError("task-title", "* This field is required");
+    isValid = false;
+  }
+
+  if (!taskdate.value.trim()) {
+    setError("due-datet", " * This field is required");
+    isValid = false;
+  }
+  return isValid;
+}
+
+function setError(fieldId, message) {
+  let input = document.getElementById(fieldId);
+  let errorDiv = document.getElementById("error-" + fieldId);
+
+  if (input) input.classList.add("input-error");
+  if (errorDiv) errorDiv.innerText = message;
+}
+
+function clearErrors() {
+  document.querySelectorAll(".error-msg").forEach((el) => {
+    el.innerText = "";
+  });
+
+  document.querySelectorAll("input").forEach((input) => {
+    input.classList.remove("input-error");
+  });
+}
+
+// const date = moment('20120222', 'YYYYMMDD').fromNow();
